00b3c87e1baac49d05c5fec3a34a88d3668e94b4
Septs <github@septs.pw>
2017-06-28 15:19:57 +0800

    simplify canvas implementation

--- src/engine/canvas-engine.js
+++ src/engine/canvas-engine.js
@@ -1,4 +1,4 @@
-const Promise = require('bluebird')
+const _ = require('lodash')
 const Canvas = require('node-require-fallback')('canvas', 'canvas-prebuilt')
 
 if (Canvas === null) throw new Error('require install "canvas" or "canvas-prebuilt".')
@@ -11,31 +11,28 @@ const toBuffer = canvas => ({
   height: canvas.height
 })
 
-const getImage = async ({contents}) => {
+const getImage = ({contents}) => {
   const image = new Canvas.Image()
   image.src = contents
   return image
 }
 
+const handleTile = (context, {x, y, offset, contents}) => {
+  context.drawImage(getImage({contents}), x + offset, y + offset)
+}
+
 module.exports = {
   async create (tiles, {width, height}) {
     const canvas = new Canvas(width, height)
-    const ctx = canvas.getContext('2d')
-    const handleTile = async ({x, y, offset, contents}) => {
-      const image = await getImage({contents})
-      ctx.drawImage(image, x + offset, y + offset)
-    }
-
-    await Promise.each(tiles, handleTile)
+    const context = canvas.getContext('2d')
+    _.forEach(tiles, _.partial(handleTile, context))
 
     return toBuffer(canvas)
   },
   async scale (base, {width, height}) {
-    const image = await getImage(base)
-
     const canvas = new Canvas(width, height)
-    const ctx = canvas.getContext('2d')
-    ctx.drawImage(image, 0, 0, width, height)
+    const context = canvas.getContext('2d')
+    context.drawImage(getImage(base), 0, 0, width, height)
 
     return toBuffer(canvas)
   }
