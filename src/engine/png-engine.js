3663a48a2dd0679b2dc25bcd8f915a58b9089e53
Septs <github@septs.pw>
2017-06-28 12:52:20 +0800

    update png-engine code

--- src/engine/png-engine.js
+++ src/engine/png-engine.js
@@ -1,31 +1,21 @@
-const Promise = require('bluebird')
+const _ = require('lodash')
 const {PNG} = require('pngjs')
+
+function handleTile (dst, tile) {
+  const src = PNG.sync.read(tile.contents)
+  PNG.bitblt(src, dst, 0, 0, src.width, src.height, tile.x + tile.offset, tile.y + tile.offset)
+}
 
 module.exports = {
   async create (tiles, options) {
-    const canvas = new PNG({width: options.width, height: options.height})
-    const handleTile = tile => {
-      const png = PNG.sync.read(tile.contents)
-      PNG.bitblt(
-        png,
-        canvas,
-        0,
-        0,
-        png.width,
-        png.height,
-        tile.x + tile.offset,
-        tile.y + tile.offset
-      )
-      return png
-    }
-    await Promise.map(tiles, handleTile)
-    return {
-      contents: PNG.sync.write(canvas),
-      type: 'png',
-      mimeType: 'image/png',
-      width: canvas.width,
-      height: canvas.height
-    }
+    const {width, height} = options
+    const canvas = new PNG({width, height})
+    _.forEach(tiles, _.partial(handleTile, canvas))
+    const contents = PNG.sync.write(canvas)
+
+    const type = 'png'
+    const mimeType = `image/${type}`
+    return {contents, type, mimeType, width, height}
   },
   async scale (base, options) {
     throw new Error('scale not supported')
