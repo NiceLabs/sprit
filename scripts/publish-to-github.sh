01b2ef406c71591cc340c8d87ff7842057aef87b
Septs <github@septs.pw>
2020-07-28 13:23:09 +0800

    add ci

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+jq '.name = "@nicelabs/sprit"' package.json > package-modified.json
+mv package-modified.json package.json
+
 VERSION=$(jq -r '.version' package.json)
 npm --no-git-tag-version version "$VERSION-$GITHUB_RUN_NUMBER"
 npm publish
