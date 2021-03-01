b12f3dd5be9221316340ecfecf4965a7422ca85f
Septs <github@septs.pw>
2021-03-01 19:40:58 +0800

    upgrade packages

--- typings/css-generator/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'css-generator' {
-  class Generator {
-    addRule(selector: string, declarationList: any): void;
-    getOutput(): string;
-  }
-
-  export function create(): Generator;
-}
